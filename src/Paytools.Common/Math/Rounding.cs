80c8223ee0b5784de91756f28ad070099857137e
paytools-uk <116939215+paytools-uk@users.noreply.github.com>
2022-11-25 14:34:33 +0000

    Added rounding up to n places

---
+++ src/Paytools.Common/Math/Rounding.cs
@@ -0,0 +1,13 @@
+﻿namespace Paytools.Common;
+
+public static class Rounding
+{
+    private static readonly decimal[] _factors = { 1, 10, 100, 1000, 10000, 100000, 1000000 };
+    public static decimal RoundUp(decimal value, int places)
+    {
+        if (places < 0 || places > 6)
+            throw new ArgumentOutOfRangeException(nameof(places), $"Rounding parameter must lie between 0 and 6");
+
+        return Math.Ceiling(value * _factors[places]) / _factors[places];
+    }
+}
